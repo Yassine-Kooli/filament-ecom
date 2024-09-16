bdc811ca8f9d79f8bf9f2c00702f5db8e5b20802
Yassine Kooli <yassine.kooli@satoripop.com>
2024-09-16 15:38:09 +0100

    Order stats

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListOrders extends ListRecords
 {
@@ -14,6 +17,35 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStats::class,
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'new' => Tab::make()
+                ->icon('heroicon-m-sparkles')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'new')),
+            'processing' => Tab::make()
+                ->icon('heroicon-o-truck')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'processing')),
+            'shipped' => Tab::make()
+                ->icon('heroicon-m-sparkles')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'shipped')),
+            'delivered' => Tab::make()
+                ->icon('heroicon-m-check-badge')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'delivered')),
+            'canceled' => Tab::make()
+                ->icon('heroicon-o-x-circle')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'canceled')),
         ];
     }
 }
