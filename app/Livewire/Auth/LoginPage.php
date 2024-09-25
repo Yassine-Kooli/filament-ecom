e799b56dc480329aebdca03e4497213df6aefd7d
Yassine Kooli <yassine.kooli@satoripop.com>
2024-09-25 15:23:45 +0100

    Signup and signin page

--- app/Livewire/Auth/LoginPage.php
+++ app/Livewire/Auth/LoginPage.php
@@ -2,10 +2,36 @@
 
 namespace App\Livewire\Auth;
 
+use Illuminate\Support\Facades\Auth;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Login')]
 class LoginPage extends Component
 {
+    public $email;
+
+    public $password;
+
+    use LivewireAlert;
+
+    public function save()
+    {
+        $this->validate([
+            'email' => 'required|email|max:255|exists:users,email',
+            'password' => 'required|min:6|max:255',
+        ]);
+        if (! Auth::attempt(['email' => $this->email, 'password' => $this->password])) {
+            session()->flash('error', 'Invalid email or password');
+
+            return;
+        }
+
+        return redirect()->intended();
+
+    }
+
     public function render()
     {
         return view('livewire.auth.login-page');
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Livewire\MyOrdersPage;
 use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
 use App\Livewire\SuccessPage;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
@@ -21,12 +22,24 @@ Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
 Route::get('/products/{slug}', ProductDetailPage::class);
 Route::get('/cart', CartPage::class);
-Route::get('/checkout', CheckoutPage::class);
-Route::get('/my-orders', MyOrdersPage::class);
-Route::get('/my-orders/{order}', MyOrderDetailPage::class);
-Route::get('/login', LoginPage::class);
-Route::get('/register', RegisterPage::class);
-Route::get('/forgot', ForgotPasswordPage::class);
-Route::get('/reset', ResetPasswordPage::class);
-Route::get('/success', SuccessPage::class);
-Route::get('/cancel', CancelPage::class);
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', LoginPage::class)->name('login');
+    Route::get('/register', RegisterPage::class);
+    Route::get('/forgot', ForgotPasswordPage::class);
+    Route::get('/reset', ResetPasswordPage::class);
+
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/logout', function () {
+        Auth::logout();
+
+        return redirect('/');
+    });
+    Route::get('/checkout', CheckoutPage::class);
+    Route::get('/my-orders', MyOrdersPage::class);
+    Route::get('/my-orders/{order}', action: MyOrderDetailPage::class);
+    Route::get('/success', SuccessPage::class);
+    Route::get('/cancel', CancelPage::class);
+});
